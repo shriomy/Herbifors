010b2c40e8a4c736228ea2acd90a76f34c80ead1
H4S1NTH4 <127721393+H4S1NTH4@users.noreply.github.com>
2025-03-02 14:49:29 +0530

    initial commit

--- consumer/temperature-consumer/src/main/java/com/example/osgi/consumer/temperature/TemperatureConsumerService.java
+++ consumer/temperature-consumer/src/main/java/com/example/osgi/consumer/temperature/TemperatureConsumerService.java
@@ -1,18 +1,30 @@
 package com.example.osgi.consumer.temperature;
 
 import com.example.osgi.producer.temperature.TemperatureService;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
-import org.osgi.framework.ServiceRegistration;
 
-public class TemperatureConsumerService {
+public class TemperatureConsumerService implements BundleActivator {
 
     private BundleContext context;
     private ServiceReference<TemperatureService> temperatureServiceReference;
     private TemperatureService temperatureService;
 
-    public TemperatureConsumerService(BundleContext context) {
+    @Override
+    public void start(BundleContext context) throws Exception {
+
         this.context = context;
+        System.out.println("Temperature Consumer started.");
+        consumeTemperature();  // Start consuming the service
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        System.out.println("Temperature Consumer stopped.");
+        if (temperatureServiceReference != null) {
+            context.ungetService(temperatureServiceReference);
+        }
     }
 
     public void consumeTemperature() {
@@ -25,11 +37,6 @@ public class TemperatureConsumerService {
             } else {
                 System.out.println("Failed to retrieve temperature service.");
             }
-        }
-    }
-    public void stop() {
-        if (temperatureServiceReference != null) {
-            context.ungetService(temperatureServiceReference);
         }
     }
 }
