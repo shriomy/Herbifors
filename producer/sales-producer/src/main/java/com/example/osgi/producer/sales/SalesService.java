63f80975d26bc39b7d6133b26edf5caec466a108
Bilal Awshid <165455288+BilalR4M@users.noreply.github.com>
2025-03-11 09:03:09 +0530

    update: sales producer

--- producer/sales-producer/src/main/java/com/example/osgi/producer/sales/SalesService.java
+++ producer/sales-producer/src/main/java/com/example/osgi/producer/sales/SalesService.java
@@ -5,4 +5,8 @@ import java.util.List;
 public interface SalesService {
     void addOrder(Order order);
     List<Order> getOrders();
+    void updateOrder(int orderId, Order order);
+    void deleteOrder(int orderId);
+    double getTotalManufacturingCost();
+    double getTotalSellingCost();
 }
